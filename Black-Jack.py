30bb75edc0fadb062d423716f15537406b0cfd6a
GRJR_1325 <97265804+GRJR1325@users.noreply.github.com>
2022-04-14 18:35:15 +0000

    Creando Baraja de juego

---
+++ Black-Jack.py
@@ -0,0 +1,10 @@
+#CREACION DE LOS VALORES DE JUEGO
+import random
+
+#MANOS DE CARTAS
+baraja = [2,3,4,5,6,7,8,9,10,2,3,4,5,6,7,8,9,10,2,3,4,5,6,7,8,9,10,2,3,4,5,6,7,8,9,10,
+          "J", "Q", "K", "A","J", "Q", "K", "A","J", "Q", "K", "A","J", "Q", "K", "A"]
+Silvie = [] 
+Jugador = []
+
+#LEE LAS CARTAS
